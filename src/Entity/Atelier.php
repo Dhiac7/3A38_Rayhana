ca4c92608d1b2ae3ae2e0ae69b16557efee8e31e
syrine <syrine.toumi@esprit.tn>
2025-02-05 12:56:19 +0100

    création de l'entité Atelier et quelques modifications

---
+++ src/Entity/Atelier.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AtelierRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert ;
+#[ORM\Entity(repositoryClass: AtelierRepository::class)]
+class Atelier
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date_atelier = null;
+
+    #[ORM\Column]
+    private ?int $capacite_max = null;
+
+    #[ORM\Column]
+    private ?float $prix = null;
+
+    #[ORM\Column]
+    private ?int $idUser = null;
+
+    #[ORM\Column(type: Types::STRING, length: 100)]
+    #[Assert\Choice(Choice: ['ouvert','complet','annulé'],message: "le statut doit étre 'ouvert','complet','annulé' ")]
+    private ?string $statut = null;
+
+    #[ORM\Column(type: Types::STRING, length: 100)]
+    #[Assert\Choice(Choice: ['agriculteur','client','employee'],message: "le role doit étre 'agriculteur','client','employee' ")]
+    private ?string $Role = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDateAtelier(): ?\DateTimeInterface
+    {
+        return $this->date_atelier;
+    }
+
+    public function setDateAtelier(\DateTimeInterface $date_atelier): static
+    {
+        $this->date_atelier = $date_atelier;
+
+        return $this;
+    }
+
+    public function getCapaciteMax(): ?int
+    {
+        return $this->capacite_max;
+    }
+
+    public function setCapaciteMax(int $capacite_max): static
+    {
+        $this->capacite_max = $capacite_max;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): static
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getIdUser(): ?int
+    {
+        return $this->idUser;
+    }
+
+    public function setIdUser(int $idUser): static
+    {
+        $this->idUser = $idUser;
+
+        return $this;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): self
+    {
+        if(!in_array($statut,['ouvert','complet','annulé'])){
+            throw new \InvalidArgumentException("le statut doit etre 'ouvert','complet','annulé' ");
+        }
+        $this->statut = $statut;
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->Role;
+    }
+
+    public function setRole(string $Role): self
+    {        if(!in_array($Role,['agriculteur','client','employee'])){
+                throw new \InvalidArgumentException("le role doit etre 'agriculteur','client','employee'");
+    }    
+                $this->Role = $Role;
+                return $this;
+
+    }
+}
