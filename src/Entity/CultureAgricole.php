646e9811b1946a880c61da2a1ef77fe425f6c1a6
Dhia <mohameddhia.chaouachi@esprit.tn>
2025-02-05 12:35:24 +0100

    creation de l'entité parcelle et cultureAgricole

---
+++ src/Entity/CultureAgricole.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CultureAgricoleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CultureAgricoleRepository::class)]
+class CultureAgricole
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $dateSemi = null;
+
+    #[ORM\Column]
+    private ?float $superficie = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $statut = null;
+
+    #[ORM\Column]
+    private ?float $rendementEstime = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDateSemi(): ?\DateTimeInterface
+    {
+        return $this->dateSemi;
+    }
+
+    public function setDateSemi(\DateTimeInterface $dateSemi): static
+    {
+        $this->dateSemi = $dateSemi;
+
+        return $this;
+    }
+
+    public function getSuperficie(): ?float
+    {
+        return $this->superficie;
+    }
+
+    public function setSuperficie(float $superficie): static
+    {
+        $this->superficie = $superficie;
+
+        return $this;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): static
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+    public function getRendementEstime(): ?float
+    {
+        return $this->rendementEstime;
+    }
+
+    public function setRendementEstime(float $rendementEstime): static
+    {
+        $this->rendementEstime = $rendementEstime;
+
+        return $this;
+    }
+}
