00243f62fc12db473ec752cdb7d42647cda87f85
mohamed kadi <mohamed.kadi@esprit.tn>
2025-02-10 10:40:01 +0100

    commit2

---
+++ src/Controller/TransactionfinancierController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Transactionfinancier;
+use App\Form\TransactionfinancierType;
+use App\Repository\TransactionfinancierRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/transactionfinancier')]
+final class TransactionfinancierController extends AbstractController{
+    #[Route(name: 'app_transactionfinancier_index', methods: ['GET'])]
+    public function index(TransactionfinancierRepository $transactionfinancierRepository): Response
+    {
+        return $this->render('transactionfinancier/index.html.twig', [
+            'transactionfinanciers' => $transactionfinancierRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_transactionfinancier_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $transactionfinancier = new Transactionfinancier();
+        $form = $this->createForm(TransactionfinancierType::class, $transactionfinancier);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($transactionfinancier);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_transactionfinancier_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('transactionfinancier/new.html.twig', [
+            'transactionfinancier' => $transactionfinancier,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_transactionfinancier_show', methods: ['GET'])]
+    public function show(Transactionfinancier $transactionfinancier): Response
+    {
+        return $this->render('transactionfinancier/show.html.twig', [
+            'transactionfinancier' => $transactionfinancier,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_transactionfinancier_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Transactionfinancier $transactionfinancier, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(TransactionfinancierType::class, $transactionfinancier);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_transactionfinancier_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('transactionfinancier/edit.html.twig', [
+            'transactionfinancier' => $transactionfinancier,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_transactionfinancier_delete', methods: ['POST'])]
+    public function delete(Request $request, Transactionfinancier $transactionfinancier, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$transactionfinancier->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($transactionfinancier);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_transactionfinancier_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
