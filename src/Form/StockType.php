3e56e9bd645b3043f0ed003d4c6378ea885442f3
Yassou055 <yassou.20005@gmail.com>
2025-02-11 14:04:19 +0100

    m

--- src/Form/StockType.php
+++ src/Form/StockType.php
@@ -21,13 +21,9 @@ class StockType extends AbstractType
         $builder
             ->add('nom', TextType::class, [
                 'label' => 'Nom du produit',
-                'attr' => [
-                    'placeholder' => 'Entrez le nom du produit',
-                ],
+                'attr' => ['placeholder' => 'Entrez le nom du produit'],
                 'constraints' => [
-                    new Assert\NotBlank([
-                        'message' => 'Le nom du produit est obligatoire'
-                    ]),
+                    new Assert\NotBlank(['message' => 'Le nom du produit est obligatoire']),
                     new Assert\Regex([
                         'pattern' => '/^[a-zA-Z\s]+$/',
                         'message' => 'Le nom doit contenir uniquement des lettres et des espaces'
@@ -42,86 +38,55 @@ class StockType extends AbstractType
             ])
             ->add('quantite', NumberType::class, [
                 'label' => 'Quantité',
-                'attr' => [
-                    'placeholder' => 'Entrez la quantité',
-                    'min' => 0
-                ],
+                'attr' => ['placeholder' => 'Entrez la quantité', 'min' => 0],
                 'constraints' => [
-                    new Assert\NotBlank([
-                        'message' => 'La quantité est obligatoire'
-                    ]),
-                    new Assert\Positive([
-                        'message' => 'La quantité doit être un nombre positif'
-                    ])
+                    new Assert\NotBlank(['message' => 'La quantité est obligatoire']),
+                    new Assert\PositiveOrZero(['message' => 'La quantité doit être un nombre positif ou zéro'])
                 ],
             ])
             ->add('date_stock', DateType::class, [
                 'label' => 'Date de stock',
                 'widget' => 'single_text',
-                'attr' => [
-                    'class' => 'form-control'
-                ],
+                'attr' => ['class' => 'form-control'],
                 'constraints' => [
-                    new Assert\NotBlank([
-                        'message' => 'La date de stock est obligatoire'
-                    ])
+                    new Assert\NotBlank(['message' => 'La date de stock est obligatoire'])
                 ],
+                'empty_data' => (new \DateTime())->format('Y-m-d'), // Valeur par défaut : aujourd'hui
             ])
             ->add('quantite_initiale', NumberType::class, [
                 'label' => 'Quantité initiale',
-                'attr' => [
-                    'placeholder' => 'Entrez la quantité initiale',
-                    'min' => 0
-                ],
+                'attr' => ['placeholder' => 'Entrez la quantité initiale', 'min' => 0],
                 'constraints' => [
-                    new Assert\NotBlank([
-                        'message' => 'La quantité initiale est obligatoire'
-                    ]),
-                    new Assert\Positive([
-                        'message' => 'La quantité initiale doit être un nombre positif'
-                    ])
+                    new Assert\NotBlank(['message' => 'La quantité initiale est obligatoire']),
+                    new Assert\PositiveOrZero(['message' => 'La quantité initiale doit être un nombre positif ou zéro'])
                 ],
             ])
             ->add('quantite_utilise', NumberType::class, [
                 'label' => 'Quantité utilisée',
-                'attr' => [
-                    'placeholder' => 'Entrez la quantité utilisée',
-                    'min' => 0
-                ],
+                'attr' => ['placeholder' => 'Entrez la quantité utilisée', 'min' => 0],
                 'constraints' => [
-                    new Assert\NotBlank([
-                        'message' => 'La quantité utilisée est obligatoire'
-                    ]),
-                    new Assert\PositiveOrZero([
-                        'message' => 'La quantité utilisée doit être un nombre positif ou zéro'
-                    ])
+                    new Assert\NotBlank(['message' => 'La quantité utilisée est obligatoire']),
+                    new Assert\PositiveOrZero(['message' => 'La quantité utilisée doit être un nombre positif ou zéro'])
                 ],
             ])
             ->add('date_expiration', DateType::class, [
                 'label' => 'Date d\'expiration',
                 'widget' => 'single_text',
-                'attr' => [
-                    'class' => 'form-control'
-                ],
+                'attr' => ['class' => 'form-control'],
                 'constraints' => [
-                    new Assert\NotBlank([
-                        'message' => 'La date d\'expiration est obligatoire'
-                    ]),
+                    new Assert\NotBlank(['message' => 'La date d\'expiration est obligatoire']),
                     new Assert\GreaterThan([
                         'value' => 'today',
                         'message' => 'La date d\'expiration doit être supérieure à la date actuelle'
                     ])
                 ],
+                'empty_data' => (new \DateTime())->modify('+1 day')->format('Y-m-d'), // Valeur par défaut : demain
             ])
             ->add('lieu', TextType::class, [
                 'label' => 'Lieu de stockage',
-                'attr' => [
-                    'placeholder' => 'Entrez le lieu de stockage'
-                ],
+                'attr' => ['placeholder' => 'Entrez le lieu de stockage'],
                 'constraints' => [
-                    new Assert\NotBlank([
-                        'message' => 'Le lieu de stockage est obligatoire'
-                    ])
+                    new Assert\NotBlank(['message' => 'Le lieu de stockage est obligatoire'])
                 ],
             ])
             ->add('conditionn', ChoiceType::class, [
@@ -133,9 +98,7 @@ class StockType extends AbstractType
                 ],
                 'placeholder' => 'Sélectionnez une condition',
                 'constraints' => [
-                    new Assert\NotBlank([
-                        'message' => 'La condition de stockage est obligatoire'
-                    ])
+                    new Assert\NotBlank(['message' => 'La condition de stockage est obligatoire'])
                 ],
             ])
             ->add('statut', ChoiceType::class, [
@@ -147,16 +110,12 @@ class StockType extends AbstractType
                 ],
                 'placeholder' => 'Sélectionnez un statut',
                 'constraints' => [
-                    new Assert\NotBlank([
-                        'message' => 'Le statut est obligatoire'
-                    ])
+                    new Assert\NotBlank(['message' => 'Le statut est obligatoire'])
                 ],
             ])
             ->add('save', SubmitType::class, [
                 'label' => 'Ajouter stock',
-                'attr' => [
-                    'class' => 'btn-submit'
-                ]
+                'attr' => ['class' => 'btn-submit']
             ]);
     }
 
@@ -164,9 +123,7 @@ class StockType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Stock::class,
-            'attr' => [
-                'novalidate' => 'novalidate', // Disable HTML5 validation to use Symfony validation
-            ]
+            'attr' => ['novalidate' => 'novalidate'], // Désactive la validation HTML5 pour utiliser Symfony
         ]);
     }
 }
