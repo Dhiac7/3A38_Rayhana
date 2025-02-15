e43951f2f2f98c52068faa473af6302987c93e94
iheb163 <ayari.iheb@esprit.tn>
2025-02-15 20:40:58 +0100

    v3.0

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Avis;
+use App\Entity\User;
 use App\Form\AvisType;
 use App\Repository\AvisRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -10,20 +11,45 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 #[Route('/avis')]
 final class AvisController extends AbstractController{
     #[Route(name: 'app_avis_index', methods: ['GET'])]
-    public function index(AvisRepository $avisRepository): Response
+    public function index(AvisRepository $avisRepository, SessionInterface $session,
+    EntityManagerInterface $entityManager): Response
     {
+        $loggedInUserId = $session->get('client_user_id');
+    
+    if (!$loggedInUserId) {
+        return $this->redirectToRoute('app_user_login');
+    }
+
+    // Récupérer l'utilisateur connecté
+    $loggedInUser = $entityManager->getRepository(User::class)->find($loggedInUserId);
+    if (!$loggedInUser) {
+        return $this->redirectToRoute('app_user_login');
+    }
         return $this->render('avis/index.html.twig', [
             'avis' => $avisRepository->findAll(),
+            'loggedInUser' => $loggedInUser,
         ]);
     }
 
     #[Route('/new', name: 'app_avis_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, SessionInterface $session): Response
     {
+        $loggedInUserId = $session->get('client_user_id');
+    
+    if (!$loggedInUserId) {
+        return $this->redirectToRoute('app_user_login');
+    }
+
+    // Récupérer l'utilisateur connecté
+    $loggedInUser = $entityManager->getRepository(User::class)->find($loggedInUserId);
+    if (!$loggedInUser) {
+        return $this->redirectToRoute('app_user_login');
+    }
         $avi = new Avis();
         $form = $this->createForm(AvisType::class, $avi);
         $form->handleRequest($request);
@@ -38,20 +64,45 @@ final class AvisController extends AbstractController{
         return $this->render('avis/new.html.twig', [
             'avi' => $avi,
             'form' => $form,
+            'loggedInUser' => $loggedInUser,
         ]);
     }
 
     #[Route('/{id}', name: 'app_avis_show', methods: ['GET'])]
-    public function show(Avis $avi): Response
+    public function show(Avis $avi, SessionInterface $session,
+    EntityManagerInterface $entityManager): Response
     {
+        $loggedInUserId = $session->get('client_user_id');
+    
+    if (!$loggedInUserId) {
+        return $this->redirectToRoute('app_user_login');
+    }
+
+    // Récupérer l'utilisateur connecté
+    $loggedInUser = $entityManager->getRepository(User::class)->find($loggedInUserId);
+    if (!$loggedInUser) {
+        return $this->redirectToRoute('app_user_login');
+    }
         return $this->render('avis/show.html.twig', [
             'avi' => $avi,
+            'loggedInUser' => $loggedInUser,
         ]);
     }
 
     #[Route('/{id}/edit', name: 'app_avis_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Avis $avi, EntityManagerInterface $entityManager): Response
     {
+        $loggedInUserId = $session->get('client_user_id');
+    
+    if (!$loggedInUserId) {
+        return $this->redirectToRoute('app_user_login');
+    }
+
+    // Récupérer l'utilisateur connecté
+    $loggedInUser = $entityManager->getRepository(User::class)->find($loggedInUserId);
+    if (!$loggedInUser) {
+        return $this->redirectToRoute('app_user_login');
+    }
         $form = $this->createForm(AvisType::class, $avi);
         $form->handleRequest($request);
 
@@ -64,11 +115,13 @@ final class AvisController extends AbstractController{
         return $this->render('avis/edit.html.twig', [
             'avi' => $avi,
             'form' => $form,
+            'loggedInUser' => $loggedInUser,
         ]);
     }
 
     #[Route('/{id}', name: 'app_avis_delete', methods: ['POST'])]
-    public function delete(Request $request, Avis $avi, EntityManagerInterface $entityManager): Response
+    public function delete(Request $request, Avis $avi,SessionInterface $session,
+    EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$avi->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($avi);
--- src/Controller/InspectionController.php
+++ src/Controller/InspectionController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Inspection;
+use App\Entity\User;
 use App\Form\InspectionType;
 use App\Repository\InspectionRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -10,20 +11,45 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 #[Route('/inspection')]
 final class InspectionController extends AbstractController{
     #[Route(name: 'app_inspection_index', methods: ['GET'])]
-    public function index(InspectionRepository $inspectionRepository): Response
+    public function index(InspectionRepository $inspectionRepository, SessionInterface $session,
+    EntityManagerInterface $entityManager): Response
     {
+        $loggedInUserId = $session->get('admin_user_id');
+    
+    if (!$loggedInUserId) {
+        return $this->redirectToRoute('app_user_loginback');
+    }
+
+    // Récupérer l'utilisateur connecté
+    $loggedInUser = $entityManager->getRepository(User::class)->find($loggedInUserId);
+    if (!$loggedInUser) {
+        return $this->redirectToRoute('app_user_loginback');
+    }
         return $this->render('inspection/index.html.twig', [
             'inspections' => $inspectionRepository->findAll(),
+            'loggedInUser' => $loggedInUser,
         ]);
     }
 
     #[Route('/new', name: 'app_inspection_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, SessionInterface $session): Response
     {
+        $loggedInUserId = $session->get('admin_user_id');
+    
+    if (!$loggedInUserId) {
+        return $this->redirectToRoute('app_user_loginback');
+    }
+
+    // Récupérer l'utilisateur connecté
+    $loggedInUser = $entityManager->getRepository(User::class)->find($loggedInUserId);
+    if (!$loggedInUser) {
+        return $this->redirectToRoute('app_user_loginback');
+    }
         $inspection = new Inspection();
         $form = $this->createForm(InspectionType::class, $inspection);
         $form->handleRequest($request);
@@ -38,20 +64,46 @@ final class InspectionController extends AbstractController{
         return $this->render('inspection/new.html.twig', [
             'inspection' => $inspection,
             'form' => $form,
+            'loggedInUser' => $loggedInUser,
         ]);
     }
 
     #[Route('/{id}', name: 'app_inspection_show', methods: ['GET'])]
-    public function show(Inspection $inspection): Response
+    public function show(Inspection $inspection, SessionInterface $session,
+    EntityManagerInterface $entityManager): Response
     {
+        $loggedInUserId = $session->get('admin_user_id');
+    
+    if (!$loggedInUserId) {
+        return $this->redirectToRoute('app_user_loginback');
+    }
+
+    // Récupérer l'utilisateur connecté
+    $loggedInUser = $entityManager->getRepository(User::class)->find($loggedInUserId);
+    if (!$loggedInUser) {
+        return $this->redirectToRoute('app_user_loginback');
+    }
         return $this->render('inspection/show.html.twig', [
             'inspection' => $inspection,
+            'loggedInUser' => $loggedInUser,
         ]);
     }
 
     #[Route('/{id}/edit', name: 'app_inspection_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Inspection $inspection, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Inspection $inspection, SessionInterface $session,
+    EntityManagerInterface $entityManager): Response
     {
+        $loggedInUserId = $session->get('admin_user_id');
+    
+    if (!$loggedInUserId) {
+        return $this->redirectToRoute('app_user_loginback');
+    }
+
+    // Récupérer l'utilisateur connecté
+    $loggedInUser = $entityManager->getRepository(User::class)->find($loggedInUserId);
+    if (!$loggedInUser) {
+        return $this->redirectToRoute('app_user_loginback');
+    }
         $form = $this->createForm(InspectionType::class, $inspection);
         $form->handleRequest($request);
 
@@ -64,6 +116,7 @@ final class InspectionController extends AbstractController{
         return $this->render('inspection/edit.html.twig', [
             'inspection' => $inspection,
             'form' => $form,
+            'loggedInUser' => $loggedInUser,
         ]);
     }
 
