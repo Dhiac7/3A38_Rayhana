e3268d4378ab582eb2d53bb31fb8ef02d6c57bcf
mohamed kadi <mohamed.kadi@esprit.tn>
2025-02-15 13:14:51 +0100

    commit14

--- src/Form/TransactionfinancierType.php
+++ src/Form/TransactionfinancierType.php
@@ -10,6 +10,8 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Positive;
 
 class TransactionfinancierType extends AbstractType
 {
@@ -21,11 +23,18 @@ class TransactionfinancierType extends AbstractType
                     'class' => 'form-control',
                     'placeholder' => 'Montant',
                 ],
+                'constraints' => [
+                    new NotBlank(['message' => 'Le montant est obligatoire.']),
+                    new Positive(['message' => 'Le montant doit être positif.']),
+                ],
             ])
             ->add('description', TextType::class, [
                 'attr' => [
                     'class' => 'form-control',
                     'placeholder' => 'Description',
+                ],
+                'constraints' => [
+                    new NotBlank(['message' => 'La description est obligatoire.']),
                 ],
             ])
             ->add('type', ChoiceType::class, [
@@ -36,11 +45,18 @@ class TransactionfinancierType extends AbstractType
                 'attr' => [
                     'class' => 'form-control',
                 ],
+                'constraints' => [
+                    new NotBlank(['message' => 'Le type est obligatoire.']),
+                ],
             ])
             ->add('nbrheure', NumberType::class, [
                 'attr' => [
                     'class' => 'form-control',
                     'placeholder' => 'Nombre d\'heures',
+                ],
+                'required' => false, // Rendre ce champ facultatif
+                'constraints' => [
+                    new Positive(['message' => 'Le nombre d\'heures doit être positif.']),
                 ],
             ]);
     }
